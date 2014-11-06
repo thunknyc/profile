dc53f856b6b9de8280cb19335f375180fb3e136a
Edwin Watkeys <edw@poseur.com>
2014-11-06 18:34:46 -0500

    remove slf4j dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "http://github.com/thunknyc/profile"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.slf4j/slf4j-jdk14 "1.7.7"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
