ad7d62121d15b74f23a9f704cd293c204860b15c
Edwin Watkeys <edw@poseur.com>
2014-11-06 18:43:57 -0500

    change project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thunknyc.profile "0.1.0-SNAPSHOT"
+(defproject thunknyc/profile "0.1.0"
   :description "A Clojure profiling library."
   :url "http://github.com/thunknyc/profile"
   :license {:name "Eclipse Public License"
