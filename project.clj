afe248ffe36a6f0aa8bca9d28dde5793f0d5cfdd
Edwin Watkeys <edw@poseur.com>
2014-11-10 12:04:39 -0500

    add codox deets

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/thunknyc/profile"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :codox {:src-dir-uri "http://github.com/thunknyc/profile/blob/master/"
+          :src-linenum-anchor-prefix "L"})
