6ae22ca32eb3184878b59e41d16637d39c29b68c
Edwin Watkeys <edw@poseur.com>
2014-11-10 11:51:34 -0500

    0.3.2, changes for marginalia

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thunknyc/profile "0.3.1"
+(defproject thunknyc/profile "0.3.2"
   :description "A Clojure profiling library."
   :url "http://github.com/thunknyc/profile"
   :license {:name "Eclipse Public License"
