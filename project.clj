f5e2f8beb7069aea1bc48a3cdf4dfcc249447511
Edwin Watkeys <edw@poseur.com>
2014-11-10 11:19:10 -0500

    version to 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thunknyc/profile "0.2.3"
+(defproject thunknyc/profile "0.3.0"
   :description "A Clojure profiling library."
   :url "http://github.com/thunknyc/profile"
   :license {:name "Eclipse Public License"
