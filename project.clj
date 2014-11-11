0c1d67adc0054d67272009bb430520beea5c8eb9
Edwin Watkeys <edw@poseur.com>
2014-11-11 09:14:02 -0500

    remove project.clj

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject thunknyc/profile "0.3.3"
-  :description "A Clojure profiling library."
-  :url "http://github.com/thunknyc/profile"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
